dagger/dagger

User defaults: persist any dagger function argument to .env
Signed-off-by: Solomon Hykes <solomon@dagger.io>

sdk/go/dagger.gen.go
@@ -8626,6 +8626,15 @@ func (r *Module) Interfaces(ctx context.Context) ([]TypeDef, error) {
 	return convert(response), nil
 }
 
+// User-defined defaults values, loaded from local .env files.
+func (r *Module) LocalDefaults() *EnvFile {
+	q := r.query.Select("localDefaults")
+
+	return &EnvFile{
+		query: q,
+	}
+}
+
 // The name of the module
 func (r *Module) Name(ctx context.Context) (string, error) {
 	if r.name != nil {
@@ -9180,6 +9189,15 @@ func (r *ModuleSource) LocalContextDirectoryPath(ctx context.Context) (string, e
 
 	q = q.Bind(&response)
 	return response, q.Execute(ctx)
+}
+
+// User-defined defaults read from local .env files
+func (r *ModuleSource) LocalDefaults() *EnvFile {
+	q := r.query.Select("localDefaults")
+
+	return &EnvFile{
+		query: q,
+	}
 }
 
 // The name of the module, including any setting via the withName API.

dagger/dagger-go-sdk

dagger.gen.go
@@ -8736,6 +8736,15 @@ func (r *Module) Sync(ctx context.Context) (*Module, error) {
 	}, nil
 }
 
+// User-defined default values, loaded from local .env files.
+func (r *Module) UserDefaults() *EnvFile {
+	q := r.query.Select("userDefaults")
+
+	return &EnvFile{
+		query: q,
+	}
+}
+
 // Retrieves the module with the given description
 func (r *Module) WithDescription(description string) *Module {
 	q := r.query.Select("withDescription")
@@ -9293,6 +9302,15 @@ func (r *ModuleSource) Sync(ctx context.Context) (*ModuleSource, error) {
 	return &ModuleSource{
 		query: q.Root().Select("loadModuleSourceFromID").Arg("id", id),
 	}, nil
+}
+
+// User-defined defaults read from local .env files
+func (r *ModuleSource) UserDefaults() *EnvFile {
+	q := r.query.Select("userDefaults")
+
+	return &EnvFile{
+		query: q,
+	}
 }
 
 // The specified version of the git repo this source points to.